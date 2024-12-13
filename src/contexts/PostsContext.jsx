fa5529f3b7e0b03ea6dfd4bb3953a9cf88397fdc
giuseppemsabino <giuseppemendoza004@gmail.com>
2024-12-13 17:26:20 +0100

    postlist in context

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -7,8 +7,56 @@ const PostsContext = createContext();
 
 // esporto il provider
 export const PostsContextProvider = ({children}) => {
-    const postlist = 'lista'
-    return <PostsContext.Provider value={postlist}>{children}</PostsContext.Provider>
+
+    const postsData = {
+        postlist: [
+        {
+            "id": 1,
+            "title": "ciambellone",
+            "content": "questa e la foto di un ciambellone",
+            "image": "/ciambellone.jpeg",
+            "category": "Food",
+            "published": true
+        },
+        {
+            "id": 2,
+            "title": "craker",
+            "content": "questa e una foto di crakers di barbabietola",
+            "image": "/cracker_barbabietola.jpeg",
+            "category": "Food",
+            "published": false
+        },
+        {
+            "id": 3,
+            "title": "pane fritto",
+            "content": "questa e la imagine di un pane fritto",
+            "image": "/pane_fritto_dolce.jpeg",
+            "category": "Dessert",
+            "published": true
+        },
+        {
+            "id": 4,
+            "title": "pasta barbabietola",
+            "content": "questa e l'imagine di pasta di barbabietola",
+            "image": "/pasta_barbabietola.jpeg",
+            "category": "Pasta",
+            "published": false
+        },
+        {
+            "id": 5,
+            "title": "torta paesana",
+            "content": "questa e l'imagine di una torta paesana",
+            "image": "/torta_paesana.jpeg",
+            "category": "Dessert",
+            "published": true
+        }
+    ]};
+    
+
+
+    return <PostsContext.Provider value={postsData}>{children}</PostsContext.Provider>
 };
 
 //esporto lo use per il consumers
+
+export const usePostsContext = () => useContext(PostsContext);
